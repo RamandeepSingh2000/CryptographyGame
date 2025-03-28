3b84adccffa08c458f031dabe694bddd1797fff7
SantiCarrenoCC <scarreno@my.centennialcollege.ca>
2025-03-28 00:27:36 -0400

    Roller coaster animation implemented, cameras switch logic implemented

---
+++ Assets/Scripts/RollerCoaster/RollerCoasterAnimationController.cs
@@ -0,0 +1,42 @@
+using Unity.Cinemachine;
+using UnityEngine;
+
+namespace RollerCoaster
+{
+    public class RollerCoasterAnimationController : MonoBehaviour
+    {
+        [SerializeField] private CinemachineSplineCart[] _splineCarts;
+        [SerializeField] private float _speed = 1f;
+        private float[] _initialOffsets;
+        private float _currentProgress = 0f;
+
+        [SerializeField] private bool _startMoving = false;
+
+        // Start is called once before the first execution of Update after the MonoBehaviour is created
+        void Start()
+        {
+            _initialOffsets = new float[_splineCarts.Length];
+            for (int i = 0; i < _splineCarts.Length; i++)
+            {
+                _initialOffsets[i] = _splineCarts[i].SplinePosition;
+            }
+        }
+
+        public void StartRide()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (!_startMoving) return;
+
+            // _currentProgress += Time.deltaTime * _speed;
+            for (int i = 0; i < _splineCarts.Length; i++)
+            {
+                var currentPosition = _splineCarts[i].SplinePosition;
+                _splineCarts[i].SplinePosition = currentPosition >= 1f ? 0f : currentPosition + Time.deltaTime * _speed;
+            }
+        }
+    }
+}
