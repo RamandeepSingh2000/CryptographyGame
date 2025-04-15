6b13ee7debd5a027187d2612b14c5b5cce47201c
Hoenirro <jorgeos@gmail.com>
2025-04-15 18:01:40 -0400

    unfocused

--- Assets/Scripts/Outline/LookAtMe.cs
+++ Assets/Scripts/Outline/LookAtMe.cs
@@ -1,15 +1,19 @@
 using System;
 using Cameras;
 using UnityEngine;
+using UnityEngine.Events;
 using UnityEngine.EventSystems;
 
 namespace CaesarCipher
 {
     public class LookAtMe : MonoBehaviour, IPointerClickHandler, IPointerEnterHandler, IPointerExitHandler
     {
+        private static LookAtMe currentLookAtMe;
         [SerializeField] private int _index;
         [SerializeField] private PlaceCamerasManager _placeCamerasManager;
         private bool _closeUp = false;
+        [SerializeField] private UnityEvent onFocus;
+        [SerializeField] private UnityEvent onUnfocus;
         private void Start()
         {
             if (_placeCamerasManager != null) return;
@@ -23,6 +27,29 @@ namespace CaesarCipher
             var k = _closeUp ? 0 : _index;
             _placeCamerasManager.SwitchToCamera(k);
             _closeUp = !_closeUp;
+            if (_closeUp)
+            {
+                onFocus?.Invoke();
+                if (currentLookAtMe != null && currentLookAtMe != this)
+                {
+                    currentLookAtMe.UnFocus();
+                }
+                currentLookAtMe = this;
+            }
+            else
+            {
+                if (currentLookAtMe != null && currentLookAtMe != this)
+                {
+                    currentLookAtMe.UnFocus();
+                }
+                onUnfocus?.Invoke();
+            }
+        }
+
+        public void UnFocus()
+        {
+            _closeUp = false;
+            onUnfocus?.Invoke();
         }
 
         public void OnPointerEnter(PointerEventData eventData)
